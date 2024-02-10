627b75048fbd6f13d7636e4946beede95f52f35a
Endriw Ventura <plmferraz@gmail.com>
2024-02-09 21:52:29 -0300

    Initial commit

---
+++ desafios-do-curso.js/desafios.js
@@ -0,0 +1,1 @@
+// Crie uma variável chamada "valor1" e outra chamada "valor2", atribuindo a elas valores numéricos de sua escolha. Em seguida, realize a soma desses dois valores e armazene o resultado em uma terceira variável chamada "resultado". Utilize o console.log para mostrar a mensagem "A soma de [valor1] e [valor2] é igual a [resultado]." no console.
